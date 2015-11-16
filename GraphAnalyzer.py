460d48d386e7c0dce5f84a26c060e2885fe3f3d6
Jordan Hurwitz <jhurwitz@sas.upenn.edu>
2015-11-15 21:55:10 -0500

    Added feature building

--- GraphAnalyzer.py
+++ GraphAnalyzer.py
@@ -1,6 +1,7 @@
 import networkx
 import glob
 import os
+import numpy
 
 
 class GraphAnalyzer(object):
@@ -8,8 +9,33 @@ class GraphAnalyzer(object):
     def __init__(self):
         super(GraphAnalyzer, self).__init__()
         self.graphs = {}
+        self.featuresByGraph = {}
+        self.features = None
+
+    def BuildFeatures(self):
+        graphs = self.graphs
+        self._ComputeMaxDegreeCentrality(graphs)
+        self._ComputeMinDegreeCentrality(graphs)
+
+        # Build X
+        features = []
+        for key in self.featuresByGraph.keys():
+            features.append(self.featuresByGraph[key])
+        self.features = numpy.array(features)
 
     def ReadGraphs(self, pathToGraphs):
         os.chdir(pathToGraphs)
         for file in glob.glob('*.gml'):
-            self.graphs[os.path.splitext(file)[0]] = networkx.read_gml(file)
+            graphName = os.path.splitext(file)[0]
+            self.graphs[graphName] = networkx.read_gml(file)
+            self.featuresByGraph[graphName] = []
+
+    def _ComputeMaxDegreeCentrality(self, graphs):
+        for key in graphs.keys():
+            degrees = networkx.degree_centrality(graphs[key])
+            self.featuresByGraph[key].append(max(degrees.iteritems())[1])
+
+    def _ComputeMinDegreeCentrality(self, graphs):
+        for key in graphs.keys():
+            degrees = networkx.degree_centrality(graphs[key])
+            self.featuresByGraph[key].append(min(degrees.iteritems())[1])
