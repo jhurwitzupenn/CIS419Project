5b0568173abd925a7223683e82e7c369c02f8a20
Jordan Hurwitz <jhurwitz@sas.upenn.edu>
2015-12-03 21:32:25 -0500

    Added Standardization

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ g.LabelFeatures()
 X = g.features
 Y = g.labels
 
+# Standardize features
+mean = X.mean(axis=0)
+std = X.std(axis=0)
+X = (X - mean) / std
+
 svm_rbf = trainClassifiers.trainSVMRBF(X, Y)
 svm_sig = trainClassifiers.trainSVMSig(X, Y)
 dtree = trainClassifiers.trainDTree(X, Y)
